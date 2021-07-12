001341f39d6caedfa323dd8f7efd10642bde53a7
daimananshen <947458050@qq.com>
2021-07-12 23:39:33 +0800

    Hello React

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// 创建外壳组件 App
+
+import React,{ Component } from "react"
+
+export default class App extends Component{
+    render(){
+        return (
+            <div>Hello React</div>
+        )
+    }
+}

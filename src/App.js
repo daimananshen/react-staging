3cf5fbf250733b593b0404ddc0b227b71b05ed2a
daimananshen <947458050@qq.com>
2021-07-13 23:05:38 +0800

    Hello组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // 创建外壳组件 App
 
 import React,{ Component } from "react"
+import Hello from './components/Hello/Hello'
+import Welcome from './components/Welcome/Welcome'
 
 export default class App extends Component{
     render(){
         return (
-            <div>Hello React</div>
+            <div>
+                <Hello/>
+                <Welcome/>
+            </div>
         )
     }
 }
